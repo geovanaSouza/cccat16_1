2f815011f757b90f4a140c68c409e7d660bed148
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 20:59:35 -0300

    Add more tests scenarios

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -8,3 +8,21 @@ test("Deve testar um cpf válido", function(){
     // then - assert
     expect(isValid).toBe(true)
 })
+
+test("Deve testar um cpf válido", function(){
+    // given - arrange
+    const cpf = "71428793860";
+    // when - act
+    const isValid = validate(cpf);
+    // then - assert
+    expect(isValid).toBe(true);
+})
+
+test("Deve testar um cpf válido", function(){
+    // given - arrange
+    const cpf = "87748248800";
+    // when - act
+    const isValid = validate(cpf);
+    // then - assert
+    expect(isValid).toBe(true)
+})

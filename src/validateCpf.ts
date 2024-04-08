63d336a133c3406854adaae59adf71ec7586fa55
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 22:04:54 -0300

    Remove try catch unreachable

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -5,25 +5,21 @@ export function validate(cpf: string) {
     cleanedCpf = cleanCpf(cpf);
     if (!isValidLength(cleanedCpf)) return false;
     if (allDigitsEqual(cleanedCpf)) return false;
-    try {
-        let d1, d2;
-        d1 = d2 = 0;
-        for (let nCount = 1; nCount < cleanedCpf.length - 1; nCount++) {
-            let digito;
-            digito = parseInt(cleanedCpf.substring(nCount - 1, nCount));
-            d1 =getD(d1, 11, nCount, digito)
-            d2 = getD(d2, 12, nCount, digito)
-        };
-        let nDigVerific = cleanedCpf.substring(cleanedCpf.length - 2, cleanedCpf.length);
-        return nDigVerific == getDigit(d1, d2);
-    } catch (e) {
-        console.error("Erro !" + e)
-        return false
-    }
+    let d1, d2;
+    d1 = d2 = 0;
+    for (let nCount = 1; nCount < cleanedCpf.length - 1; nCount++) {
+        let digito;
+        digito = parseInt(cleanedCpf.substring(nCount - 1, nCount));
+        d1 = getD(d1, 11, nCount, digito)
+        d2 = getD(d2, 12, nCount, digito)
+    };
+    let nDigVerific = cleanedCpf.substring(cleanedCpf.length - 2, cleanedCpf.length);
+    return nDigVerific == getDigit(d1, d2);
+
 }
 
 function cleanCpf(cpf: string) {
-    return cpf.replace(/\D/g,"");
+    return cpf.replace(/\D/g, "");
 }
 
 function isValidLength(cpf: string) {
@@ -34,11 +30,11 @@ function allDigitsEqual(cpf: string) {
     return (cpf.split("").every(c => c === cpf[0]))
 }
 
-function getD(d: number, factor: number, count: number, digito: number){
+function getD(d: number, factor: number, count: number, digito: number) {
     return d + (factor - count) * digito;
 }
 
-function getDigit(d1: number, d2: number){
+function getDigit(d1: number, d2: number) {
     let rest
     rest = (d1 % 11);
     let dg1

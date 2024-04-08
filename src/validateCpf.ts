40e134c8015a294429397b17f1131149b3463209
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 21:34:32 -0300

    refactor(validatecpf): improve code
    Improve variable names and move definition close to usage

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,33 +1,34 @@
-export function validate(str: string) {
-    if (str === null) return false;
-    if (str === undefined) return false;
-    if (!isCpfLenValid(str)) return false;
-    str = cleanCpf(str);
-    if (isCpfDigitValid(str)) return false;
-
+export function validate(cpf: string) {
+    if (cpf === null) return false;
+    if (cpf === undefined) return false;
+    if (!isCpfLenValid(cpf)) return false;
+    let cleanedCpf;
+    cleanedCpf = cleanCpf(cpf);
+    if (isCpfDigitValid(cleanedCpf)) return false;
     try {
         let d1, d2;
-        let dg1, dg2, rest;
-        let digito;
-        let nDigResult;
         d1 = d2 = 0;
-        dg1 = dg2 = rest = 0;
-        for (let nCount = 1; nCount < str.length - 1; nCount++) {
-            digito = parseInt(str.substring(nCount - 1, nCount));
+        for (let nCount = 1; nCount < cleanedCpf.length - 1; nCount++) {
+            let digito;
+            digito = parseInt(cleanedCpf.substring(nCount - 1, nCount));
             d1 = d1 + (11 - nCount) * digito;
             d2 = d2 + (12 - nCount) * digito;
         };
+        let rest;
         rest = (d1 % 11);
         // se for menor que 2 é 0, senão é 11 menos o resto
+        let dg1;
         dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
         d2 += 2 * dg1;
         rest = (d2 % 11);
+        let dg2;
         if (rest < 2)
             dg2 = 0;
         else
             dg2 = 11 - rest;
 
-        let nDigVerific = str.substring(str.length - 2, str.length);
+        let nDigVerific = cleanedCpf.substring(cleanedCpf.length - 2, cleanedCpf.length);
+        let nDigResult;
         nDigResult = "" + dg1 + "" + dg2;
         return nDigVerific == nDigResult;
         // se der problema...

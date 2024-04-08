6dd4b4362eabdb4015ea7a8f780ab9d04526b896
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 22:09:16 -0300

    Use ternary to improve code

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -42,9 +42,6 @@ function getDigit(d1: number, d2: number) {
     d2 += 2 * dg1;
     rest = (d2 % 11);
     let dg2;
-    if (rest < 2)
-        dg2 = 0;
-    else
-        dg2 = 11 - rest;
+    dg2 = (rest < 2) ? 0 : (11 - rest);
     return "" + dg1 + "" + dg2;
 }

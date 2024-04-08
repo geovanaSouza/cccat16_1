519338afc59a8a9e0d798bb84c7a067ba431c1d2
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 21:27:32 -0300

    refactor(validatecpf): improve code
    Remove blank lines and dead code. Improve ciclomatic complexitty using early return. Remove comments
    using good functions names

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,58 +1,54 @@
-export function validate (str: string){
-    if (str !== null){
-        if (str !== undefined){
-            if (str.length >= 11 && str.length <= 14){
-                // cleaning cpf
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");
-                // tudo igual
-                if (!str.split("").every(c => c === str[0])){
-                    try{
-                        let d1,d2;
-                        let dg1, dg2, rest;
-                        let digito;
-                            let nDigResult;
-                        d1 = d2 = 0;
-                        dg1 = dg2 = rest = 0;
+export function validate(str: string) {
+    if (str === null) return false;
+    if (str === undefined) return false;
+    if (!isCpfLenValid(str)) return false;
+    str = cleanCpf(str);
+    if (isCpfDigitValid(str)) return false;
 
-                        for (let nCount = 1; nCount < str.length -1; nCount++){
-                            // if (isNan(parseInt(str.substring(nCount -1, nCount)))) {
-                                // retunr false;
-                            // } else {
-                                digito = parseInt(str.substring(nCount -1, nCount));
-                                d1 = d1 + (11 - nCount) * digito;
+    try {
+        let d1, d2;
+        let dg1, dg2, rest;
+        let digito;
+        let nDigResult;
+        d1 = d2 = 0;
+        dg1 = dg2 = rest = 0;
+        for (let nCount = 1; nCount < str.length - 1; nCount++) {
+            digito = parseInt(str.substring(nCount - 1, nCount));
+            d1 = d1 + (11 - nCount) * digito;
+            d2 = d2 + (12 - nCount) * digito;
+        };
+        rest = (d1 % 11);
+        // se for menor que 2 é 0, senão é 11 menos o resto
+        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+        d2 += 2 * dg1;
+        rest = (d2 % 11);
+        if (rest < 2)
+            dg2 = 0;
+        else
+            dg2 = 11 - rest;
 
-                                d2 = d2 + (12 - nCount) * digito;
-                            // }
-                        };
-                        rest = (d1 % 11);
+        let nDigVerific = str.substring(str.length - 2, str.length);
+        nDigResult = "" + dg1 + "" + dg2;
+        return nDigVerific == nDigResult;
+        // se der problema...
+    } catch (e) {
+        console.error("Erro !" + e)
+        return false
+    }
+}
 
-                        // se for menor que 2 é 0, senão é 11 menos o resto
-                        dg1 = (rest <2) ? dg1 =0 : 11 - rest;
-                        d2 += 2 * dg1;
-                        rest = (d2 % 11);
-                        if (rest < 2)
-                            dg2 = 0;
-                        else
-                            dg2 = 11 - rest;
+function isCpfLenValid(cpf: string) {
+    return cpf.length >= 11 && cpf.length <= 14
+}
 
-                            let nDigVerific = str.substring(str.length-2, str.length);
-                        nDigResult = "" + dg1 + "" + dg2;
+function cleanCpf(cpf: string) {
+    return cpf
+        .replace('.', '')
+        .replace('.', '')
+        .replace('-', '')
+        .replace(" ", "");
+}
 
-                        return nDigVerific == nDigResult;
-
-                    // se der problema...
-                    }catch(e){
-                        console.error("Erro !"+e)
-
-                        return false
-                    }
-                } else return false;
-
-            }else return false;
-        } else return false;
-    } else return false;
+function isCpfDigitValid(cpf: string) {
+    return (cpf.split("").every(c => c === cpf[0]))
 }

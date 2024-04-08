425228f975dbbe9dcfb85b1410a5a3df712d9d03
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-07 21:04:29 -0300

    Improve tests readbility

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -5,6 +5,5 @@ test.each([
     "71428793860",
     "87748248800"
 ])("Deve testar um cpf válido: %s", function(cpf: any){
-    const isValid = validate(cpf);
-    expect(isValid).toBe(true)
+    expect(validate(cpf)).toBe(true)
 })

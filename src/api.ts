63db1715299a59cfc992a5da40de18659da3e71b
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-08 23:12:09 -0300

    Implement early return

--- src/api.ts
+++ src/api.ts
@@ -15,50 +15,48 @@ app.post("/signup", async function (req, res) {
 		const id = crypto.randomUUID();
 
 		const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
-		if (!acc) {
+		if (acc) return res.status(422).send(-4 + "");
 
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
 
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
+		if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
+			if (req.body.email.match(/^(.+)@(.+)$/)) {
+
+				if (validate(req.body.cpf)) {
+					if (req.body.isDriver) {
+						if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
 							await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
 
 							const obj = {
 								accountId: id
 							};
 							result = obj;
+						} else {
+							// invalid car plate
+							result = -5;
 						}
 					} else {
-						// invalid cpf
-						result = -1;
+						await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+
+						const obj = {
+							accountId: id
+						};
+						result = obj;
 					}
 				} else {
-					// invalid email
-					result = -2;
+					// invalid cpf
+					result = -1;
 				}
-
 			} else {
-				// invalid name
-				result = -3;
+				// invalid email
+				result = -2;
 			}
 
 		} else {
-			// already exists
-			result = -4;
+			// invalid name
+			result = -3;
 		}
+
+
 		if (typeof result === "number") {
 			res.status(422).send(result + "");
 		} else {

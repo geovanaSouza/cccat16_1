93e12f61cc1e64a9234ed7bd05751d52ef51eacd
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-14 19:51:43 -0300

    refactor(try/finally): remove try/finally

--- src/api.ts
+++ src/api.ts
@@ -10,46 +10,40 @@ var validator = require('validator');
 
 app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const [accountExistent] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
-		if (accountExistent) return res.status(HttpStatusCode.BadRequest).send(-4 + "");
-		if (!isNameValid(req.body.name)) return res.status(HttpStatusCode.BadRequest).send(-3 + "");
-		if (!isEmailValid(req.body.email)) return res.status(HttpStatusCode.BadRequest).send(-2 + "");
-		if (!isCpfValid(req.body.cpf)) return res.status(HttpStatusCode.BadRequest).send(-1 + "");
-		if ((req.body.isDriver) && (!isPlateValid(req.body.carPlate))) return res.status(HttpStatusCode.BadRequest).send(-5 + "");
-		const accountID = crypto.randomUUID();
-		await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountID, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-		const accountCreated = {
-			accountId: accountID
-		};
-		res.json(accountCreated);
-	} finally {
-		await connection.$pool.end();
-	}
+	const [accountExistent] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
+	if (accountExistent) return res.status(HttpStatusCode.BadRequest).send(-4 + "");
+	if (!isNameValid(req.body.name)) return res.status(HttpStatusCode.BadRequest).send(-3 + "");
+	if (!isEmailValid(req.body.email)) return res.status(HttpStatusCode.BadRequest).send(-2 + "");
+	if (!isCpfValid(req.body.cpf)) return res.status(HttpStatusCode.BadRequest).send(-1 + "");
+	if ((req.body.isDriver) && (!isPlateValid(req.body.carPlate))) return res.status(HttpStatusCode.BadRequest).send(-5 + "");
+	const accountID = crypto.randomUUID();
+	await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountID, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+	const accountCreated = {
+		accountId: accountID
+	};
+	await connection.$pool.end();
+	res.json(accountCreated);
 });
 
-function isNameValid (name: string) {
+function isNameValid(name: string) {
 	return name.match(/[a-zA-Z] [a-zA-Z]+/)
 }
 
-function isEmailValid(email: string){
+function isEmailValid(email: string) {
 	return email.match(/^(.+)@(.+)$/)
 }
 
-function isPlateValid(plate :string){
+function isPlateValid(plate: string) {
 	return plate.match(/[A-Z]{3}[0-9]{4}/)
 }
 
 app.get("/accounts/:accountId", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		if (validator.isUUID(req.params.accountId) !== true) return res.status(HttpStatusCode.BadRequest).send(-2 + "");
-		const [account] = await connection.query("select * from cccat16.account where account_id = $1", [req.params.accountId]);
-		if (!account) return res.status(HttpStatusCode.NotFound).send(-1 + "");
-		res.json(account);
-	} finally {
-		await connection.$pool.end();
-	}
+	if (validator.isUUID(req.params.accountId) !== true) return res.status(HttpStatusCode.BadRequest).send(-2 + "");
+	const [account] = await connection.query("select * from cccat16.account where account_id = $1", [req.params.accountId]);
+	if (!account) return res.status(HttpStatusCode.NotFound).send(-1 + "");
+	await connection.$pool.end();
+	res.json(account);
 });
 
 app.listen(3000);

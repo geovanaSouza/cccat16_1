df25b0f011c32b707b3fa96f0744b211f433347e
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-14 16:49:24 -0300

    refactor(signup): remove cyclometic complexity
    Use early return pattern to improve the code

--- src/api.ts
+++ src/api.ts
@@ -13,55 +13,18 @@ app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
-
 		const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
 		if (acc) return res.status(422).send(-4 + "");
-
-
-		if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-			if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-				if (validate(req.body.cpf)) {
-					if (req.body.isDriver) {
-						if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-							await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						} else {
-							// invalid car plate
-							result = -5;
-						}
-					} else {
-						await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-						const obj = {
-							accountId: id
-						};
-						result = obj;
-					}
-				} else {
-					// invalid cpf
-					result = -1;
-				}
-			} else {
-				// invalid email
-				result = -2;
-			}
-
-		} else {
-			// invalid name
-			result = -3;
-		}
-
-
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
+		if (!req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) return res.status(422).send(-3 + "");
+		if (!req.body.email.match(/^(.+)@(.+)$/)) return res.status(422).send(-2 + "");
+		if (!validate(req.body.cpf)) return res.status(422).send(-1 + "");
+		if ((req.body.isDriver) && (!req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/))) return res.status(422).send(-5 + "");
+		await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+		const obj = {
+			accountId: id
+		};
+		result = obj;
+		res.json(result);
 	} finally {
 		await connection.$pool.end();
 	}

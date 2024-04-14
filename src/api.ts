26d133ee67b4b1dd1f341cd605e7c881e77a1606
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-14 20:42:08 -0300

    refactor(signup): remove duplicated code when return is failure

--- src/api.ts
+++ src/api.ts
@@ -11,18 +11,24 @@ var validator = require('validator');
 app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	const [accountExistent] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
-	if (accountExistent) return res.status(HttpStatusCode.BadRequest).send(-4 + "");
-	if (!isNameValid(req.body.name)) return res.status(HttpStatusCode.BadRequest).send(-3 + "");
-	if (!isEmailValid(req.body.email)) return res.status(HttpStatusCode.BadRequest).send(-2 + "");
-	if (!isCpfValid(req.body.cpf)) return res.status(HttpStatusCode.BadRequest).send(-1 + "");
-	if (req.body.isDriver && !isCarPlateValid(req.body.carPlate)) return res.status(HttpStatusCode.BadRequest).send(-5 + "");
+	let result;
+	if (accountExistent) result = -4;
+	if (!isNameValid(req.body.name)) result = -3;
+	if (!isEmailValid(req.body.email)) result = -2;
+	if (!isCpfValid(req.body.cpf)) result = -1;
+	if (req.body.isDriver && !isCarPlateValid(req.body.carPlate)) result = -5;
 	const accountID = crypto.randomUUID();
 	await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountID, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-	const accountCreated = {
-		accountId: accountID
-	};
+	if (typeof result === "number"){
+		res.status(HttpStatusCode.BadRequest).send(result + "");
+	} else {
+		const accountCreated = {
+			accountId: accountID
+		};
+		res.json(accountCreated);
+	}
 	await connection.$pool.end();
-	res.json(accountCreated);
+	
 });
 
 function isNameValid(name: string) {

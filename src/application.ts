b9bac5d7570d12640e9cbb1933435d01bf5c8f43
Geovana Possenti <geovana.possenti@ifood.com.br>
2024-04-14 22:24:38 -0300

    refactor(segregate resources): use database interaction from resources

--- src/application.ts
+++ src/application.ts
@@ -1,28 +1,22 @@
 import crypto from "crypto";
-import pgp from "pg-promise";
 import { isCpfValid } from "./validateCpf";
+import { getAccountByEmail, getAccountById, saveAccount } from "./resource";
 
 var validator = require('validator');
 
 export async function signup(input: any): Promise<any> {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const [accountExistent] = await connection.query("select * from cccat16.account where email = $1", [input.email]);
-		if (accountExistent) return -4;
-		if (!isNameValid(input.name)) return -3;
-		if (!isEmailValid(input.email)) return -2;
-		if (!isCpfValid(input.cpf)) return -1;
-		if (input.isDriver && !isCarPlateValid(input.carPlate)) return -5;
-		const accountID = crypto.randomUUID();
-		await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountID, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-		const accountCreated = {
-			accountId: accountID
-		};
-		return accountCreated;
-	} finally {
-		await connection.$pool.end();
+	const account = input;
+	account.accountId = crypto.randomUUID();
+	const accountExistent = await getAccountByEmail(input.email);
+	if (accountExistent) return -4;
+	if (!isNameValid(account.name)) return -3;
+	if (!isEmailValid(account.email)) return -2;
+	if (!isCpfValid(account.cpf)) return -1;
+	if (account.isDriver && !isCarPlateValid(account.carPlate)) return -5;
+	await saveAccount(account);
+	return {
+		accountId: account.accountId
 	};
-
 };
 
 function isNameValid(name: string) {
@@ -41,13 +35,7 @@ function isCarPlateValid(carPlate: string) {
 }
 
 export async function getAccount(accountId: string) {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const [account] = await connection.query("select * from cccat16.account where account_id = $1", [accountId]);
-		if (!account) return -1;
-		return account;
-	} finally {
-		await connection.$pool.end();
-	};
-
+	const account = await getAccountById(accountId);
+	if (!account) return -1;
+	return account;
 }
